349197ba337d2fbde888c24ebfeca80d706ad7b2
fvlingen <>
2006-10-16 23:34:58 +0000

    Added some more exception codes.
    Frank.

--- src/python/ProdCommon/Core/Codes.py
+++ src/python/ProdCommon/Core/Codes.py
@@ -5,7 +5,7 @@ exceptions={4001:'General database error',\
             4002:'No connection when trying to start transaction',\
             4003:'No transaction has started when trying to get a cursor',\
             4004:'Unknown error',\
-            4005:'No connection when trying to commit',\
+            4005:'No transaction was started when trying to commit',\
            }
 
 
