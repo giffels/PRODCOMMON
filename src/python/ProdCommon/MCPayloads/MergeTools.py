4759f38dfa1d55c6bbb380e01905551a42e96bb0
evansde <>
2007-04-03 16:27:04 +0000

    Add prototype createMergeJobWorkflow method

--- src/python/ProdCommon/MCPayloads/MergeTools.py
+++ src/python/ProdCommon/MCPayloads/MergeTools.py
@@ -9,7 +9,8 @@ Common tools for generating and managing merging workflows
 import copy
 from ProdCommon.MCPayloads.WorkflowSpec import WorkflowSpec
 from ProdCommon.MCPayloads.DatasetInfo import DatasetInfo
-
+import ProdCommon.MCPayloads.DatasetConventions as DatasetConventions
+import ProdCommon.MCPayloads.WorkflowTools as WorkflowTools
 
 class ProcToMerge:
     """
@@ -75,5 +76,93 @@ def createMergeDatasetWorkflow(procSpec, isFastMerge = True):
     return newSpec
 
 
+
+def createMergeJobWorkflow(procSpec, isFastMerge = True, doCleanUp = True):
+    """
+    _createMergeJobWorkflow_
+
+    Given a Processing Workflow, generate a set of Merge Job
+    workflows that can be used to generate actual merge jobs 
+    (as opposed to creating datasets like createMergeDatasetWorkflow)
+
+    returns a dictionary of (input, IE MergeSensor watched) dataset name
+    to workflow spec instances
+
+    """
+    mergeDatasetWF = createMergeDatasetWorkflow(procSpec, isFastMerge)
+    mergeDatasets = mergeDatasetWF.outputDatasets()
+
+    results = {}
+
+    procSpecName = procSpec.workflowName()
     
-    
+
+    for dataset in mergeDatasets:
+        inputDataset = dataset['ParentDataset']
+        name = "%s-%s-Merge" % (procSpecName, dataset['OutputModuleName'])
+
+        newWF = WorkflowSpec()
+        newWF.parameters.update(procSpec.parameters)
+        newWF.setWorkflowName(name)
+        newWF.parameters['WorkflowType'] = "Merge"
+
+
+        cmsRunNode = newWF.payload
+        cmsRunNode.name = "cmsRun1"
+        cmsRunNode.type = "CMSSW"
+        cmsRunNode.application["Project"] = "CMSSW"
+        cmsRunNode.application["Version"] = dataset['ApplicationVersion']
+        cmsRunNode.application["Architecture"] = "slc3_ia32_gcc323"
+
+        if isFastMerge == True:
+            cmsRunNode.application["Executable"] = "EdmFastMerge"
+            outputModuleName = "EdmFastMerge"
+        else:
+            cmsRunNode.application["Executable"] = "cmsRun"
+            outputModuleName = "Merged"
+
+        #  //
+        # // Input Dataset
+        #//
+        datasetBits = DatasetConventions.parseDatasetPath(inputDataset)
+        inDataset = cmsRunNode.addInputDataset(datasetBits['Primary'],
+                                               datasetBits['Processed'])
+        inDataset["DataTier"] = datasetBits['DataTier']
+
+        #  //
+        # // Output Dataset
+        #//
+        
+        outputDataset = cmsRunNode.addOutputDataset(
+            dataset['PrimaryDataset'], 
+            dataset['ProcessedDataset'], 
+            outputModuleName)
+
+        outputDataset["DataTier"] = dataset['DataTier']
+        outputDataset["PSetHash"] = dataset['PSetHash']
+
+        outputDataset["ApplicationName"] = \
+                    cmsRunNode.application["Executable"]
+        outputDataset["ApplicationProject"] = \
+                    cmsRunNode.application["Project"]
+        outputDataset["ApplicationVersion"] = \
+                    cmsRunNode.application["Version"]
+        outputDataset["ApplicationFamily"] = outputModuleName
+        outputDataset["PhysicsGroup"] = \
+                      procSpec.parameters.get('PhysicsGroup', None)
+        outputDataset['ParentDataset'] = inputDataset
+                
+        
+        #  //
+        # // Add Stage Out node
+        #//
+        WorkflowTools.addStageOutNode(cmsRunNode, "stageOut1")
+        if doCleanUp == True:
+            WorkflowTools.addCleanUpNode(cmsRunNode, "cleanUp1")
+
+        WorkflowTools.generateFilenames(newWF)
+
+        
+        results[inputDataset] = newWF
+
+    return results
