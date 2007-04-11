595d6625fa8d638dd648af3d68aa7d4b38d6e0f1
evansde <>
2007-04-11 16:32:57 +0000

    Changes to WorkflowMaker: default pileup files per job is now 10, add validateDataset method that calls out to DBS to check a dataset exists and that the name is valid

--- src/python/ProdCommon/MCPayloads/WorkflowMaker.py
+++ src/python/ProdCommon/MCPayloads/WorkflowMaker.py
@@ -20,6 +20,9 @@ from ProdCommon.MCPayloads.LFNAlgorithm import unmergedLFNBase, mergedLFNBase
 from ProdCommon.CMSConfigTools.CfgInterface import CfgInterface
 from ProdCommon.MCPayloads.UUID import makeUUID
 
+from ProdCommon.DataMgmt.DBS.DBSReader import DBSReader
+
+
 
 class WorkflowMakerError(ProdException):
     """
@@ -223,7 +226,7 @@ class WorkflowMaker:
         return
         
 
-    def addPileupDataset(self, datasetName, filesPerJob = 1):
+    def addPileupDataset(self, datasetName, filesPerJob = 10):
         """
         _addPileupDataset_
 
@@ -397,5 +400,41 @@ class WorkflowMaker:
 
         return
 
+    def validateDataset(self, datasetPath, dbsUrl):
+        """
+        _validateDataset_
 
-    
+        Util method to check that the datasetPath provided
+        exists in the dbsUrl provided
+
+        """
+        
+        datasetDetails = DatasetConventions.parseDatasetPath(datasetPath)
+        for key in ['Primary', 'DataTier', 'Processed']:
+            if datasetDetails[key] == None:
+                msg = "Invalid Dataset Name: \n ==> %s\n" % datasetPath
+                msg += "Does not contain %s information" % key
+                raise WorkflowMakerError(msg)
+                
+
+        datasets = []
+        try:
+            reader = DBSReader(dbsUrl)
+            datasets = reader.matchProcessedDatasets(
+                datasetDetails['Primary'],
+                datasetDetails['DataTier'],
+                datasetDetails['Processed'])
+        except Exception, ex:
+            msg = "Error calling DBS to validate dataset:\n%s\n" % datasetPath
+            msg += str(ex)
+            raise WorkflowMakerError(msg)
+            
+
+        if len(datasets) == 0:
+            msg = "No matching datasets found for path:\n ==> %s\n" % (
+                datasetPath,
+                )
+            msg += " In DBS Instance:\n ==> %s\n" % dbsUrl
+            raise WorkflowMakerError(msg)
+
+        return 
