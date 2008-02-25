3827a9e56ebc0e695080a2273b5620bb1e29d15e
afanfani <>
2008-02-25 21:21:01 +0000

    add jt tables

--- src/python/ProdCommon/BossLite/DbObjects/setupDatabase.sql
+++ src/python/ProdCommon/BossLite/DbObjects/setupDatabase.sql
@@ -76,3 +76,28 @@ CREATE TABLE bl_runningjob
   )
   TYPE = InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE jt_group 
+  (
+  id int(11) NOT NULL auto_increment,
+  group_id int(11) default NULL,
+  task_id int(11) NOT NULL,
+  job_id int(11) NOT NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY task_id (task_id,job_id),
+  KEY job_id (job_id)
+  ) 
+  TYPE = InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE jt_activejobs 
+  (
+  job_id varchar(255) NOT NULL,
+  status enum('output_not_requested','output_requested','in_progress',
+       'output_retrieved','output_processed') default 'output_not_requested',
+  directory text,
+  output text,
+  boss_status varchar(10) default '',
+  job_spec_id varchar(255) default NULL,
+  UNIQUE KEY job_id (job_id)
+  ) 
+  TYPE = InnoDB DEFAULT CHARSET=latin1;
+
