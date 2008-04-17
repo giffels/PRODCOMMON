0dee38d51ae66d3949e5be3a342393d83b68dc24
ewv <>
2008-04-17 14:25:38 +0000

    Move some JDL lines to CRAB side

--- src/python/ProdCommon/BossLite/Scheduler/SchedulerGlidein.py
+++ src/python/ProdCommon/BossLite/Scheduler/SchedulerGlidein.py
@@ -3,8 +3,8 @@
 _SchedulerGlidein_
 """
 
-__revision__ = "$Id: SchedulerCondorGAPI.py,v 1.22 2008/04/15 17:01:25 ewv Exp $"
-__version__ = "$Revision: 1.22 $"
+__revision__ = "$Id: SchedulerGlidein.py,v 1.1 2008/04/15 22:00:15 ewv Exp $"
+__version__ = "$Revision: 1.1 $"
 
 from ProdCommon.BossLite.Scheduler.SchedulerCondorCommon import SchedulerCondorCommon
 
@@ -16,7 +16,7 @@ class SchedulerGlidein(SchedulerCondorCommon) :
 
     # call super class init method
     super(SchedulerGlidein, self).__init__(user_proxy)
-  
+
   def singleApiJdl( self, job, requirements='' ):
       """
       build a job jdl easy to be handled by the wmproxy API interface
@@ -26,12 +26,12 @@ class SchedulerGlidein(SchedulerCondorCommon) :
       jdl = 'Universe   = vanilla\n'
       superJdl, filelist = super(SchedulerCondorG, self).singleApiJdl(job, requirements)
       jdl += superJdl
-      
+
       glidein = 'UCSDT2'
-      
-      jdl += '+DESIRED_Sites = "'+glidein+'\n'
-      jdl += '+DESIRED_Archs = "INTEL,X86_64"\n'
-      jdl += 'Requirements = stringListMember(GLIDEIN_Site,DESIRED_Sites) && stringListMember(Arch, DESIRED_Archs) \n'
+
+      #jdl += '+DESIRED_Sites = "'+glidein+'\n'
+      #jdl += '+DESIRED_Archs = "INTEL,X86_64"\n'
+      #jdl += 'Requirements = stringListMember(GLIDEIN_Site,DESIRED_Sites) && stringListMember(Arch, DESIRED_Archs) \n'
       #jdl += '+JOB_Site = \"\$\$(GLIDEIN_Site:Unknown)\" \n");
       #jdl += '+JOB_VM = \"\$\$(Name:Unknown)\" \n");
       #jdl += '+JOB_Machine_KFlops = \"\$\$(KFlops:Unknown)\" \n");
@@ -42,6 +42,6 @@ class SchedulerGlidein(SchedulerCondorCommon) :
       #jdl += '+JOB_GLIDEIN_Factory = \"\$\$(GLIDEIN_Factory:Unknown)\" \n");
       #jdl += '+JOB_GLIDEIN_Name = \"\$\$(GLIDEIN_Name:Unknown)\" \n");
       #jdl += '+JOB_GLIDEIN_Frontend = \"\$\$(GLIDEIN_Client:Unknown)\" \n");
-     
+
       return jdl, filelist
 
