2dd3c374945f990bb76c0d6b5ecd0b8a48d67f3a
ewv <>
2008-04-17 21:50:38 +0000

    Enable other variable

--- src/python/ProdCommon/BossLite/Scheduler/SchedulerGlidein.py
+++ src/python/ProdCommon/BossLite/Scheduler/SchedulerGlidein.py
@@ -3,8 +3,8 @@
 _SchedulerGlidein_
 """
 
-__revision__ = "$Id: SchedulerGlidein.py,v 1.3 2008/04/17 19:20:55 ewv Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: SchedulerGlidein.py,v 1.4 2008/04/17 21:32:39 ewv Exp $"
+__version__ = "$Revision: 1.4 $"
 
 from ProdCommon.BossLite.Scheduler.SchedulerCondorCommon import SchedulerCondorCommon
 
@@ -28,16 +28,16 @@ class SchedulerGlidein(SchedulerCondorCommon) :
       jdl += superJdl
 
       # Optional parameters that may be put in after the conversion from Perl to Python and escapes are fixed
-      #jdl += '+JOB_Site = \"\$\$(GLIDEIN_Site:Unknown)\" \n");
-      #jdl += '+JOB_VM = \"\$\$(Name:Unknown)\" \n");
+      jdl += '+JOB_Site = "$$(GLIDEIN_Site:Unknown)" \n'
+      jdl += '+JOB_VM = "$$(Name:Unknown)" \n'
       #jdl += '+JOB_Machine_KFlops = \"\$\$(KFlops:Unknown)\" \n");
       #jdl += '+JOB_Machine_Mips = \"\$\$(Mips:Unknown)\" \n");
-      #jdl += '+JOB_GLIDEIN_Schedd = \"\$\$(GLIDEIN_Schedd:Unknown)\" \n");
-      #jdl += '+JOB_GLIDEIN_ClusterId = \"\$\$(GLIDEIN_ClusterId:Unknown)\" \n");
-      #jdl += '+JOB_GLIDEIN_ProcId = \"\$\$(GLIDEIN_ProcId:Unknown)\" \n");
-      #jdl += '+JOB_GLIDEIN_Factory = \"\$\$(GLIDEIN_Factory:Unknown)\" \n");
-      #jdl += '+JOB_GLIDEIN_Name = \"\$\$(GLIDEIN_Name:Unknown)\" \n");
-      #jdl += '+JOB_GLIDEIN_Frontend = \"\$\$(GLIDEIN_Client:Unknown)\" \n");
+      jdl += '+JOB_GLIDEIN_Schedd = "$$(GLIDEIN_Schedd:Unknown)" \n'
+      jdl += '+JOB_GLIDEIN_ClusterId = "$$(GLIDEIN_ClusterId:Unknown)" \n'
+      jdl += '+JOB_GLIDEIN_ProcId = "$$(GLIDEIN_ProcId:Unknown)" \n'
+      jdl += '+JOB_GLIDEIN_Factory = "$$(GLIDEIN_Factory:Unknown)" \n'
+      jdl += '+JOB_GLIDEIN_Name = "$$(GLIDEIN_Name:Unknown)" \n'
+      jdl += '+JOB_GLIDEIN_Frontend = "$$(GLIDEIN_Client:Unknown)" \n'
 
       return jdl, filelist
 
