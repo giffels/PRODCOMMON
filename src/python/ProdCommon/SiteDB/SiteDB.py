e3fe3c992459593374cfc5cbb8ca484ca2b01658
ewv <>
2008-07-08 18:59:04 +0000

    Replace eval with dictionary parser

--- src/python/ProdCommon/SiteDB/SiteDB.py
+++ src/python/ProdCommon/SiteDB/SiteDB.py
@@ -6,12 +6,16 @@ API for dealing with retrieving information from SiteDB
 
 """
 
-SITEDB_JSON="https://cmsweb.cern.ch/sitedb/sitedb/json/index/"
-
-
 import urllib
+import cStringIO
+import tokenize
 
-def getJSON(service, **args):
+class SiteDBJSON:
+
+  SITEDB_JSON="https://cmsweb.cern.ch/sitedb/sitedb/json/index/"
+
+
+  def getJSON(self,service, **args):
     """
     _getJSON_
 
@@ -19,25 +23,70 @@ def getJSON(service, **args):
     argument dictionaries
 
     """
-    query = SITEDB_JSON
+    query = self.SITEDB_JSON
     query += "%s" % service
 
-    
-    
     params = urllib.urlencode(args)
     f = urllib.urlopen(query, params)
     result = f.read()
     f.close()
-    #
-    # TODO: replace eval with something less dangerous, probably need to 
-    # build a parser for the dict format only here.
-    output = eval(result, {}, {})
+
+    output = self.dictParser(result)
     return output
-    
+
+
+  def _parse(self,token, src):
+    """
+    Dictionary string parser from
+    Fredrik Lundh (fredrik at pythonware.com)
+    on python-list
+    """
+    if token[1] == "{":
+      out = {}
+      token = src.next()
+      while token[1] != "}":
+        key = self._parse(token, src)
+        token = src.next()
+        if token[1] != ":":
+	  raise SyntaxError("malformed dictionary")
+        value = self._parse(src.next(), src)
+        out[key] = value
+        token = src.next()
+        if token[1] == ",":
+	  token = src.next()
+      return out
+    elif token[1] == "[":
+      out = []
+      token = src.next()
+      while token[1] != "]":
+        out.append(_parse(token, src))
+        token = src.next()
+        if token[1] == ",":
+          token = src.next()
+      return out
+    elif token[0] == tokenize.STRING:
+      return token[1][1:-1].decode("string-escape")
+    elif token[0] == tokenize.NUMBER:
+      try:
+        return int(token[1], 0)
+      except ValueError:
+        return float(token[1])
+    else:
+      raise SyntaxError("malformed expression")
+
+
+  def dictParser(self,source):
+     src = cStringIO.StringIO(source).readline
+     src = tokenize.generate_tokens(src)
+     return self._parse(src.next(), src)
+
+
 
 if __name__ == '__main__':
 
-    print getJSON("dnUserName", dn="/C=UK/O=eScience/OU=Bristol/L=IS/CN=simon metson")
-    
-    print getJSON("CEtoCMSName", name="a01-004-128.gridka.de")
-    
+    mySiteDB = SiteDBJSON()
+
+    print mySiteDB.getJSON("dnUserName", dn="/C=UK/O=eScience/OU=Bristol/L=IS/CN=simon metson")
+
+    print mySiteDB.getJSON("CEtoCMSName", name="cmsosgce.fnal.gov")
+
