48d433402d6a859d5bac2685fff4c546f1c41559
ewv <>
2008-09-08 19:21:56 +0000

    Prepare for merge with HEAD

--- src/python/ProdCommon/BossLite/Scheduler/SchedulerCondorG.py
+++ src/python/ProdCommon/BossLite/Scheduler/SchedulerCondorG.py
@@ -3,8 +3,8 @@
 _SchedulerCondorG_
 """
 
-__revision__ = "$Id: SchedulerCondorG.py,v 1.2 2008/04/29 08:15:42 gcodispo Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: SchedulerCondorG.py,v 1.2.2.1 2008/07/31 20:07:31 ewv Exp $"
+__version__ = "$Revision: 1.2.2.1 $"
 
 from ProdCommon.BossLite.Scheduler.SchedulerCondorCommon import SchedulerCondorCommon
 
@@ -12,13 +12,12 @@ class SchedulerCondorG(SchedulerCondorCommon) :
   """
   basic class to handle glite jobs through wmproxy API
   """
+
   def __init__( self, **args ):
 
-    # call super class init method
-    super(SchedulerCondorG, self).__init__(**args)
-    print "chedulerCondorG::init"
+      # call super class init method
+      super(SchedulerCondorG, self).__init__(**args)
 
-  
 
   def singleApiJdl( self, job, requirements='' ):
       """
@@ -26,10 +25,7 @@ class SchedulerCondorG(SchedulerCondorCommon) :
       and gives back the list of input files for a better handling
       """
 
-      print "Leave SchedulerCondorCommon::singleApiJdl" 
       jdl = 'Universe   = grid\n'
       superJdl, filelist = super(SchedulerCondorG, self).singleApiJdl(job, requirements)
       jdl += superJdl
       return jdl, filelist
-      print "Leave SchedulerCondorCommon::singleApiJdl" 
-
