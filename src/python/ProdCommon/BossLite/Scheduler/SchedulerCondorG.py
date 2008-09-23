f128ada402bedb16a56405ce6e389130966ccc10
ewv <>
2008-09-23 13:07:37 +0000

    clean up code from pylint warnings

--- src/python/ProdCommon/BossLite/Scheduler/SchedulerCondorG.py
+++ src/python/ProdCommon/BossLite/Scheduler/SchedulerCondorG.py
@@ -3,29 +3,29 @@
 _SchedulerCondorG_
 """
 
-__revision__ = "$Id: SchedulerCondorG.py,v 1.2 2008/04/29 08:15:42 gcodispo Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: SchedulerCondorG.py,v 1.3 2008/09/09 14:55:49 ewv Exp $"
+__version__  = "$Revision: 1.3 $"
 
 from ProdCommon.BossLite.Scheduler.SchedulerCondorCommon import SchedulerCondorCommon
 
 class SchedulerCondorG(SchedulerCondorCommon) :
-  """
-  basic class to handle glite jobs through wmproxy API
-  """
-  def __init__( self, **args ):
+    """
+    basic class to handle glite jobs through wmproxy API
+    """
 
-    # call super class init method
-    super(SchedulerCondorG, self).__init__(**args)
+    def __init__( self, **args ):
+        # call super class init method
+        super(SchedulerCondorG, self).__init__(**args)
 
 
-  def singleApiJdl( self, job, requirements='' ):
-      """
-      build a job jdl easy to be handled by the wmproxy API interface
-      and gives back the list of input files for a better handling
-      """
+    def singleApiJdl( self, job, requirements='' ):
+        """
+        build a job jdl easy to be handled by the wmproxy API interface
+        and gives back the list of input files for a better handling
+        """
 
-      jdl = 'Universe   = grid\n'
-      superJdl, filelist, ce = super(SchedulerCondorG, self).singleApiJdl(job, requirements)
-      jdl += superJdl
-      return jdl, filelist, ce
-
+        jdl = 'Universe   = grid\n'
+        superJdl, filelist, ce \
+            = super(SchedulerCondorG, self).singleApiJdl(job, requirements)
+        jdl += superJdl
+        return jdl, filelist, ce
