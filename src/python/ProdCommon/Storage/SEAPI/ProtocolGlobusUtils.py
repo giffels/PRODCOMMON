f775e4b7795047368ba8d4194c79a4778c0666d0
mcinquil <>
2009-11-03 14:52:48 +0000

    Removing double logout, replacing tabs with spaces

--- src/python/ProdCommon/Storage/SEAPI/ProtocolGlobusUtils.py
+++ src/python/ProdCommon/Storage/SEAPI/ProtocolGlobusUtils.py
@@ -13,12 +13,12 @@ class ProtocolGlobusUtils(Protocol):
 
     def __init__(self):
         super(ProtocolGlobusUtils, self).__init__()
-	# set verbose option
-	self.options = " -vb " 
+        # set verbose option
+        self.options = " -vb " 
 
     def simpleOutputCheck(self, outLines):
         """
-        parse line by line the outLines text lookng for Exceptions
+        parse line by line the outLines text looking for Exceptions
         """
         problems = []
         lines = outLines.split("\n")
@@ -55,42 +55,42 @@ class ProtocolGlobusUtils(Protocol):
 
     def lessSimpleOutputCheck(self, outLines, sources):
         """
-        parse line by line the outLines text lookng for Exceptions
+        parse line by line the outLines text looking for Exceptions
         """ 
 
-	errcodeList = ["0"]*len(sources)
+        errcodeList = ["0"]*len(sources)
         problems = [] 
-	problemsList = [""]*len(sources)
-	sourceIndex = 0
+        problemsList = [""]*len(sources)
+        sourceIndex = 0
 
         lines = outLines.split("\n")
         for line in lines:
             line = line.lower()
-	    # determine which source in the list the output
-	    # is going to be for.  Errors for that source will be 
-	    # added into the correct element in the problems list
-	    if line.find("source") != -1:
-		sourceIndex += 1		
+             # determine which source in the list the output
+             # is going to be for.  Errors for that source will be 
+             # added into the correct element in the problems list
+            if line.find("source") != -1:
+                sourceIndex += 1
 
 
             if line.find("no entries for host") != -1 or\
                line.find("srm client error") != -1:
                 #raise MissingDestination("Host not found!", [line], outLines)
-		problemsList[sourceIndex] += "Host not found! \n" + line
-		errcodeList[sourceIndex] = 1
+                problemsList[sourceIndex] += "Host not found! \n" + line
+                errcodeList[sourceIndex] = 1
 
             elif line.find("user has no permission") != -1 or\
                  line.find("permission denied") != -1:
                 #raise AuthorizationException("Permission denied!", \
                 #                              [line], outLines)
-		problemsList[sourceIndex] += "Permission denied! \n" + line
-		errcodeList[sourceIndex] = 2
+                problemsList[sourceIndex] += "Permission denied! \n" + line
+                errcodeList[sourceIndex] = 2
 
             elif line.find("file exists") != -1:
                 #raise AlreadyExistsException("File already exists!", \
                 #                              [line], outLines)
-		problemsList[sourceIndex] += "File already exists! \n" + line
-		errcodeList[sourceIndex] = 3
+                problemsList[sourceIndex] += "File already exists! \n" + line
+                errcodeList[sourceIndex] = 3
 
             elif line.find("no such file or directory") != -1 or \
                line.find("error") != -1 or line.find("failed") != -1 or \
@@ -99,37 +99,37 @@ class ProtocolGlobusUtils(Protocol):
                 cacheP = line.split(":")[-1]
                 if cacheP not in problems:
                     problems.append(cacheP)
-		    problemsList[sourceIndex] += cacheP
-		    errcodeList[sourceIndex] = 4
-	
+                    problemsList[sourceIndex] += cacheP
+                    errcodeList[sourceIndex] = 4
+
             elif line.find("unknown option") != -1 or \
                  line.find("unrecognized option") != -1 or \
                  line.find("invalid option") != -1:
                 #raise WrongOption("Wrong option passed to the command", \
                 #                  [line], outLines)
-		problemsList[sourceIndex] += "Wrong option passed to the command\n" + line
-		errcodeList[sourceIndex] = 5
+                problemsList[sourceIndex] += "Wrong option passed to the command\n" + line
+                errcodeList[sourceIndex] = 5
 
             elif line.find("command not found") != -1:
                 #raise MissingCommand("Command not found: client not " \
                 #                     "installed or wrong environment", \
                 #                     [line], outLines)
-		problemsList[sourceIndex] += "Command not found: client not installed or wrong environment\n" + line
-		errcodeList[sourceIndex] = 6
+                problemsList[sourceIndex] += "Command not found: client not installed or wrong environment\n" + line
+                errcodeList[sourceIndex] = 6
 
-	# errors are printed before the word "Source" comes up, so
-	# the sourceIndex is actually the number of sources that were
-	# encountered.  So it maps directly to index starting from zero.
+        # errors are printed before the word "Source" comes up, so
+        # the sourceIndex is actually the number of sources that were
+        # encountered.  So it maps directly to index starting from zero.
 
-	if sourceIndex < (len(sources)):
-		while sourceIndex < (len(sources)):
-			problemsList[sourceIndex] += "copy not attempted"
-			errcodeList[sourceIndex] = -1
-			sourceIndex += 1
+        if sourceIndex < (len(sources)):
+            while sourceIndex < (len(sources)):
+                problemsList[sourceIndex] += "copy not attempted"
+                errcodeList[sourceIndex] = -1
+                sourceIndex += 1
 
-	problemsTuple = []
-	for t in map(None, errcodeList, problemsList):
-		problemsTuple.append(t)#[0] + "\n " + t[1])
+        problemsTuple = []
+        for t in map(None, errcodeList, problemsList):
+            problemsTuple.append(t)#[0] + "\n " + t[1])
 
         return problemsTuple
 
@@ -166,31 +166,31 @@ class ProtocolGlobusUtils(Protocol):
         import tempfile
         import os
 
-	# sources list
+        # sources list
         sourcesList = []
         if type(source.workon) == StringType or \
            type(source.workon) == UnicodeType:
             sourcesList.append(source.getLynk())
         else:
-	    allSources = source.workon
+            allSources = source.workon
             for onesource in allSources:
-		source.workon = onesource
-		sourcesList.append(source.getLynk())
-	    # put back to how it was
-	    source.workon = allSources
+                source.workon = onesource
+                sourcesList.append(source.getLynk())
+            # put back to how it was
+            source.workon = allSources
 
-	# destinations list
+        # destinations list
         destsList   = []
         if type(dest.workon) == StringType or \
            type(dest.workon) == UnicodeType:
             destsList = [dest.getLynk()]*len(sourcesList)
         else:
-	    allDests = dest.workon
+            allDests = dest.workon
             for onedest in allDests:
-		dest.workon = onedest
+                dest.workon = onedest
                 destsList.append(dest.getLynk())
-	    # put back to how it was
-	    dest.workon = allDests
+            # put back to how it was
+            dest.workon = allDests
 
         setProxy = ''  
         if proxy is not None:
@@ -202,19 +202,18 @@ class ProtocolGlobusUtils(Protocol):
         # create the list of source, destination pairs and write to the temp file
         toCopy = "\n".join([t[0] + " " + t[1] for t in map(None, sourcesList, destsList)]) + "\n"
         try:
-	    # make a temporary file to contain the list of source, destination pairs
+            # make a temporary file to contain the list of source, destination pairs
             tmp, fname = tempfile.mkstemp( "", "seapi_", os.getcwd() )
             os.close( tmp )
             file(fname, 'w').write( toCopy )
-	    # construct the copy command with the tempfile as the argument
+            # construct the copy command with the tempfile as the argument
             cmd = setProxy + " globus-url-copy -vb -cd -f " + fname
-	    # do the copy and log the output
+            # do the copy and log the output
             exitcode, outputs = self.executeCommand(cmd)
-            super(ProtocolGlobusUtils, self).__logout__(cmd, exitcode, outputs)
         finally:
-	    # remove the temp file
+            # remove the temp file
             os.unlink( fname )
-            
+ 
         ### simple output parsing ###
         #problems = self.simpleOutputCheck(outputs)
         resvalList = self.lessSimpleOutputCheck(outputs, sourcesList)
@@ -223,7 +222,7 @@ class ProtocolGlobusUtils(Protocol):
         #if exitcode != 0 or oneOrMoreProblems:
             #raise TransferException("Error copying " + toCopy, resvalList, outputs )
 
-	return resvalList
+        return resvalList
 
     def move(self, source, dest, proxy = None, opt = ""):
         """
