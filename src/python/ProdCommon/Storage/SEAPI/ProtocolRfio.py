bef012c3659e2f448845b79cebdf1584fed15e76
riahi <>
2010-02-04 18:05:13 +0000

    Fix for bug #62366: Add control to ignore empty string

--- src/python/ProdCommon/Storage/SEAPI/ProtocolRfio.py
+++ src/python/ProdCommon/Storage/SEAPI/ProtocolRfio.py
@@ -192,9 +192,10 @@ class ProtocolRfio(Protocol):
  
         outt = []
         for out in outputs.split("\n"):
-            fileout = out.split()
-            fileout[3] = self.__convertPermission__(out[3])
-            outt.append( fileout ) 
+            if out:
+               fileout = out.split()
+               fileout[3] = self.__convertPermission__(out[3])
+               outt.append( fileout ) 
         ### need to parse the output of the commands ###
         
         return outt
